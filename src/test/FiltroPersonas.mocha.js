bb1aeb16bf3583720400e863ef266044d0e148ca
rene <renedp1975@gmail.com>
2018-10-15 12:02:34 -0500

    added more data to json static file

--- src/test/FiltroPersonas.mocha.js
+++ src/test/FiltroPersonas.mocha.js
@@ -33,13 +33,13 @@ describe('filtroPersonas() function tests', () => {
 
         const resultado_esperado = [
             { 
-                "id": 1, "name": "Rene Dominguez", "age": 100,  "image": "avatar.jpg" , 
-                 "languages": [{"id": 1, "name": "Java"},{"id": 4, "name": "C#"},{"id": 9, "name": "PHP"},{"id": 12, "name": "Objective-C"}] ,
-                 "description" : "AAA Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "     
+                "id": 8, "name": "Susan Green", "age": 56,  "image": "susan.png" ,
+                 "languages": [{"id": 1, "name": "Java"}, {"id": 19, "name": "Visual Basic"},{"id": 4, "name": "C#"}, {"id": 10, "name": "MatLab"},{"id": 19, "name": "Visual Basic"}],
+                 "description" : "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "           
                }
         ]
       
-        filtro_nombres='rene';
+        filtro_nombres='susan';
         let lista_final=filtroPersonas(lista_inicial, filtro_nombres, fil_Lenguajes, ageMin, ageMax);
         expect(lista_final).to.eql(resultado_esperado);
 
@@ -59,7 +59,12 @@ it('Age filter test', () => {
              "id": 5, "name": "Jhon Doe", "age": 20,  "image": "elliot.jpg" ,
               "languages": [{"id": 1, "name": "Java"}, {"id": 19, "name": "Visual Basic"},{"id": 4, "name": "C#"},{"id": 13, "name": "Assembly"}],
               "description" : "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "           
-            }       
+            } ,
+            { 
+                "id": 6, "name": "Jake Taylor", "age": 24,  "image": "jane.png" ,
+                 "languages": [{"id": 1, "name": "Java"}, {"id": 19, "name": "Visual Basic"},{"id": 4, "name": "C#"}, {"id": 10, "name": "MatLab"},{"id": 19, "name": "Visual Basic"}],
+                 "description" : "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "           
+               }      
     ]
   
     
@@ -77,22 +82,17 @@ it('Skills filter test', () => {
 
     const resultado_esperado = [
         { 
-             "id": 1, "name": "Rene Dominguez", "age": 100,  "image": "avatar.jpg" , 
-            "languages": [{"id": 1, "name": "Java"},{"id": 4, "name": "C#"},{"id": 9, "name": "PHP"},{"id": 12, "name": "Objective-C"}] ,
-            "description" : "AAA Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "     
-        },
-        { 
-            "id": 5, "name": "Jhon Doe", "age": 20,  "image": "elliot.jpg" ,
-            "languages": [{"id": 1, "name": "Java"}, {"id": 19, "name": "Visual Basic"},{"id": 4, "name": "C#"},{"id": 13, "name": "Assembly"}],
-            "description" : "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "           
-        }       
+            "id": 1, "name": "Don Miller", "age": 100,  "image": "avatar.jpg" , 
+             "languages": [{"id": 1, "name": "Java"},{"id": 4, "name": "C#"},{"id": 9, "name": "PHP"},{"id": 12, "name": "Objective-C"}] ,
+             "description" : "AAA Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "     
+           }   
     ]
   
     
     ageMin = 0;
     ageMax = 0;
     filtro_nombres='';
-    fil_Lenguajes =[{"id": 1, "name": "Java"},{"id": 4, "name": "C#"}];
+    fil_Lenguajes =[{"id": 1, "name": "Java"},{"id": 4, "name": "C#"}, {"id": 9, "name": "PHP"}];
 
     let lista_final=filtroPersonas(lista_inicial, filtro_nombres, fil_Lenguajes, ageMin, ageMax);
     expect(lista_final).to.eql(resultado_esperado);
